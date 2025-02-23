d326200d01fc32dbf52a0de81684db5401c4d43a
ribmen <v.g.menezes100@gmail.com>
2025-02-23 15:31:26 -0300

    feat: atualizando links pops

--- src/components/body/Pops/Pops.tsx
+++ src/components/body/Pops/Pops.tsx
@@ -134,13 +134,57 @@ export const Pops: React.FC = () => {
           </FlexContainer>
         </Section>
         <Section>
-          <SectionTitle>Acesso aos conteúdos</SectionTitle>
+          <Title>Acesso aos conteúdos</Title>
           <TextContent>
             <p>Acesse, clicando nos LINKS a seguir, os respectivos Procedimentos Operacionais Padrão para os setores comercial e administrativo:</p>
+            <SectionTitle>POPs COMERCIAL</SectionTitle>
+            {/* <ul style={{textDecoration: 'none'}}>
+              <li style={{textDecoration: 'none'}}><h3>CARGO</h3></li>
+            </ul> */}
+            <h3>VENDEDOR</h3>
             <ul>
-              <li><NavLink to='/popscomercial'><strong>POPs COMERCIAL</strong></NavLink></li>
-              <li><NavLink to='/popsadministrativo'><strong>POPs ADMINISTRATIVO</strong></NavLink></li>
+              <li><p>LINK VENDA VIA P2E: <a href="https://drive.google.com/file/d/1PeopBtIRhBPhEbTFhcg8gFAuQxLlVxWL/view?usp=sharing" target="_blank"><u>https://drive.google.com/file/d/1PeopBtIRhBPhEbTFhcg8gFAuQxLlVxWL/view?usp=sharing</u></a></p></li>
+
+              <li><p>LINK COMPRA SEMINOVOS: <a href="https://drive.google.com/file/d/1_T9LiStRSVYfMSEB7qXcTIRETtavF_p3/view?usp=sharing" target="_blank"><u>https://drive.google.com/file/d/1_T9LiStRSVYfMSEB7qXcTIRETtavF_p3/view?usp=sharing</u></a></p></li>
+              
+              <li><p>LINK HONDA STORE: <a href="https://drive.google.com/file/d/1SSxQ-6qJ020lFxCg9jmQ3jccoloUlhbU/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/1SSxQ-6qJ020lFxCg9jmQ3jccoloUlhbU/view?usp=sharing</u></a></p></li>
+              
+              <li><p>LINK EFETIVAÇÃO: <a href="https://drive.google.com/file/d/15_K3ZdQtmzD83DU-vAz-5JzrukmxSPdh/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/15_K3ZdQtmzD83DU-vAz-5JzrukmxSPdh/view?usp=sharing</u></a></p></li>
+              </ul>
+
+            <h3>AUXILIAR DE VENDAS</h3>
+            <ul>
+              <li><p>LINK VENDA NO CLOUD: <a href="https://drive.google.com/file/d/1S_7Byox2jbLzB84yiak9ddX9xYZaU8K1/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/1S_7Byox2jbLzB84yiak9ddX9xYZaU8K1/view?usp=sharing</u></a></p></li>
             </ul>
+
+            <h3>COORDENADOR DE LOGÍSTICA</h3>
+            <ul>
+              <li><p>LINK PROCESSO DE ENTRADA DE NOTAS FISCAIS: <a href="https://drive.google.com/file/d/17R9PthSLhxrULe6OT8ZqwuIdLW-oXu7G/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/17R9PthSLhxrULe6OT8ZqwuIdLW-oXu7G/view?usp=sharing</u></a></p></li>
+
+              <li><p>LINK PROCESSO DE TRANSFERÊNCIA DE MOTOS ENTRE LOJAS: <a href="https://drive.google.com/file/d/12feGzi0qbrvWVXrrxQ76X5q45rpGvryu/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/12feGzi0qbrvWVXrrxQ76X5q45rpGvryu/view?usp=sharing</u></a></p></li>
+
+            </ul>
+
+            <h3>F&I</h3>
+            <ul>
+              <li><p>LINK FINANCIAMENTO: <a href="https://drive.google.com/file/d/1321OBR9I43RbgLBAfw5YFlPSd21_oCaC/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/1321OBR9I43RbgLBAfw5YFlPSd21_oCaC/view?usp=sharing</u></a></p></li>
+
+              <li><p>LINK SEGURO: <a href="https://drive.google.com/file/d/1TrqZefxwOR5vS_-KiI5EZ467K0Jv1gnZ/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/1TrqZefxwOR5vS_-KiI5EZ467K0Jv1gnZ/view?usp=sharing</u></a></p></li>
+
+            </ul>
+
+
+            <SectionTitle>POPs ADMINISTRATIVO</SectionTitle>
+            <h3>PÓS-VENDA</h3>
+            <ul>
+              <li><p>LINK ADIMPLÊNCIA:<a href="https://drive.google.com/file/d/1utv0LGeludQNbu9C5AavA01ysLzHKFel/view?usp=sharing" target="_blank"><u> https://drive.google.com/file/d/1utv0LGeludQNbu9C5AavA01ysLzHKFel/view?usp=sharing</u></a></p></li>
+              <li><p>LINK BORDERÔ: <a href="https://drive.google.com/file/d/13yAlBAMneThDzuoFj7NPJh-zB5l7oDH0/view?usp=sharing" target="_blank"> <u>https://drive.google.com/file/d/13yAlBAMneThDzuoFj7NPJh-zB5l7oDH0/view?usp=sharing</u></a></p></li>
+              <li><p>LINK CANCELAMENTO: <a href="https://drive.google.com/file/d/1erdeEjQVY3id17zALFhqQi0wRx-QgSbn/view?usp=sharing" target="_blank"><u>https://drive.google.com/file/d/1erdeEjQVY3id17zALFhqQi0wRx-QgSbn/view?usp=sharing</u></a></p></li>
+              <li><p>LINK RETENÇÃO: <a href="https://drive.google.com/file/d/1RL29cMIdkkbt-u5v-Qf69k2UkND3uzgG/view?usp=sharing" target="_blank"><u>https://drive.google.com/file/d/1RL29cMIdkkbt-u5v-Qf69k2UkND3uzgG/view?usp=sharing</u></a></p></li>
+            </ul>
+
+            <h3>ADMINISTRATIVO HSF</h3>
+
           </TextContent>
         </Section>
 
