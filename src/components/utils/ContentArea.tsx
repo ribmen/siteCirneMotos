0fa03c461a44a99b04526ed87cf14774ccf4aaee
ribmen <v.g.menezes100@gmail.com>
2025-02-24 21:52:42 -0300

    resolving visibility início

--- src/components/utils/ContentArea.tsx
+++ src/components/utils/ContentArea.tsx
@@ -1,11 +1,9 @@
-// ContentArea.tsx
 import React, { useEffect, useState, useRef } from 'react';
 import styled, { CSSProperties } from 'styled-components';
 
 interface ContentAreaProps {
   children: React.ReactNode;
   style?: CSSProperties;
-  
 }
 
 const ContentAreaWrapper = styled.div<{ isvisible: boolean }>`
@@ -19,40 +17,46 @@ const ContentAreaWrapper = styled.div<{ isvisible: boolean }>`
   transform: translateY(-20px);
   transition: opacity 0.5s ease, transform 0.5s ease;
 
-  
   /* Animação quando o componente estiver visível */
   ${({ isvisible }) => isvisible && `
     opacity: 1;
     transform: translateY(0);
-    `}
+  `}
 
-    @media screen and (max-width: 932px) {
-      width: 88%;
-    }
+  @media screen and (max-width: 932px) {
+    width: 88%;
+  }
 `;
 
 const ContentArea: React.FC<ContentAreaProps> = ({ children, style }) => {
   const [isvisible, setisvisible] = useState(false);
   const contentRef = useRef<HTMLDivElement | null>(null);
 
-  // Detecta se o componente entrou na tela
-  const handleScroll = () => {
-    if (contentRef.current) {
-      const rect = contentRef.current.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-        setisvisible(true);
-      } 
-    }
-  };
-
   useEffect(() => {
-    // Adiciona o evento de scroll para detectar o posicionamento
-    window.addEventListener('scroll', handleScroll);
-    // Chama uma vez no início para verificar a visibilidade
-    handleScroll();
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setisvisible(true);
+            observer.unobserve(entry.target); // Para de observar após a primeira vez
+          }
+        });
+      },
+      {
+        root: null, // Observa a janela do navegador
+        rootMargin: '0px', // Margem ao redor do root
+        threshold: 0.2, // Considera visível quando 10% do elemento estiver na tela
+      }
+    );
+
+    if (contentRef.current) {
+      observer.observe(contentRef.current);
+    }
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      if (contentRef.current) {
+        observer.unobserve(contentRef.current);
+      }
     };
   }, []);
 
